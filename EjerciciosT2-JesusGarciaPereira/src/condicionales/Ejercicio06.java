c953f401e0a2b02a1c25b38691f7cfff94f968c0
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-03 14:41:27 +0100

    En proceso

--- EjerciciosT2-JesusGarciaPereira/src/condicionales/Ejercicio06.java
+++ EjerciciosT2-JesusGarciaPereira/src/condicionales/Ejercicio06.java
@@ -12,16 +12,26 @@ public class Ejercicio06 {
 	 * solución es 54 + 96 = 150
 	 */
 	public static void main(String[] args) {
+		// Declaramos las variables de los números aleatorios que generará el programa
 		int aleatorio1, aleatorio2;
+		// Declaramos la variable de la suma que le pediremos al usuario
 		int suma;
+		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
+		// Presentamos el programa
 		System.out.println("Juego de la suma");
 		System.out.println("Mostraré dos números aleatorios e introducirás el valor de la suma");
 		System.out.println("Pulsa INTRO para empezar");
 		sc.nextLine();
+		// Creamos un objeto de tipo Random
 		Random r = new Random();
+		/*
+		 * Asisnamos a cada una de estas variables un número entero aleatorio del 1 al
+		 * 99
+		 */
 		aleatorio1 = r.nextInt(1, 100);
 		aleatorio2 = r.nextInt(1, 100);
+		// Mostramos las dos variables
 		System.out.println(aleatorio1 + " + " + aleatorio2 + " = ¿?");
 		System.out.println("Introduzca la solución");
 		suma = sc.nextInt();
