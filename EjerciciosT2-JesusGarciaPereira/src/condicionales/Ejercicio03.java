d0ef6ba5b9f232ef8331bbe79cf2f73d4df34c57
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-05 19:11:13 +0100

    Excepción del primero atrapado y en bucle

--- EjerciciosT2-JesusGarciaPereira/src/condicionales/Ejercicio03.java
+++ EjerciciosT2-JesusGarciaPereira/src/condicionales/Ejercicio03.java
@@ -47,83 +47,89 @@ public class Ejercicio03 {
 		double kilosComidosPorAnimal;
 		// Declaramos una variable boolean para determinar si hay comida
 		boolean hayComida;
+		boolean compradoCorrecto = false;
 		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
 		// Para poder escribir en la consola decimales con punto
 		sc.useLocale(Locale.US);
 		// Presentamos el programa
 		System.out.println("Granja");
-		try {
-			// Le pedimos al usuario la cantidad de comida comprada
-			System.out.println(
-					"Introduzca la cantidad de comida comprada hoy (en kg y, si tiene decimales, use un punto como en el ejemplo 100.5)");
-			// Leemos el número
-			comidaComprada = sc.nextDouble();
-			/*
-			 * Si el número se sale del margen le indicamos el error y pediremos otro todas
-			 * las veces necesarias hasta que introduzca un número correcto
-			 */
-			while (comidaComprada < 0) {
-				System.out.println("Dato incorrecto, sólo validos números desde el 0");
-				System.out.println("Introduzca otro número");
+		do {
+			try {
+				// Le pedimos al usuario la cantidad de comida comprada
+				System.out.println(
+						"Introduzca la cantidad de comida comprada hoy (en kg y, si tiene decimales, use un punto como en el ejemplo 100.5)");
+				// Leemos el número
 				comidaComprada = sc.nextDouble();
-			}
-			// Le pedimos al usuario el número de animales
-			System.out.println("Introduzca el número de animales que hay en la granja");
-			// Leemos el número
-			numAnimales = sc.nextInt();
-			/*
-			 * Si el número se sale del margen le indicamos el error y pediremos otro todas
-			 * las veces necesarias hasta que introduzca un número correcto
-			 */
-			while (numAnimales < 0) {
-				System.out.println("Dato incorrecto, sólo validos números desde el 0");
-				System.out.println("Introduzca otro número");
+				compradoCorrecto = true;
+				/*
+				 * Si el número se sale del margen le indicamos el error y pediremos otro todas
+				 * las veces necesarias hasta que introduzca un número correcto
+				 */
+				while (comidaComprada < 0) {
+					System.out.println("Dato incorrecto, sólo validos números desde el 0");
+					System.out.println("Introduzca otro número");
+					comidaComprada = sc.nextDouble();
+				}
+				// Le pedimos al usuario el número de animales
+				System.out.println("Introduzca el número de animales que hay en la granja");
+				// Leemos el número
 				numAnimales = sc.nextInt();
-			}
-			// Le pedimos al usuario los kilos consumidos en total por los animales
-			System.out.println(
-					"Introduzca los kilos de comida que comen todos los animales a diario (si tiene decimales, recuerde usar un punto)");
-			// Leemos el número
-			kilosComidosTotal = sc.nextDouble();
-			/*
-			 * Si el número se sale del margen le indicamos el error y pediremos otro todas
-			 * las veces necesarias hasta que introduzca un número correcto
-			 */
-			while (kilosComidosTotal <= 0) {
-				System.out.println("Dato incorrecto, sólo validos números mayores que 0");
-				System.out.println("Introduzca otro número");
+				/*
+				 * Si el número se sale del margen le indicamos el error y pediremos otro todas
+				 * las veces necesarias hasta que introduzca un número correcto
+				 */
+				while (numAnimales < 0) {
+					System.out.println("Dato incorrecto, sólo validos números desde el 0");
+					System.out.println("Introduzca otro número");
+					numAnimales = sc.nextInt();
+				}
+				// Le pedimos al usuario los kilos consumidos en total por los animales
+				System.out.println(
+						"Introduzca los kilos de comida que comen todos los animales a diario (si tiene decimales, recuerde usar un punto)");
+				// Leemos el número
 				kilosComidosTotal = sc.nextDouble();
-			}
-			/*
-			 * Si la cantidad de comida comprada es igual o superior a la consumida por los
-			 * animales, el valor de hayComida es true; por el contrario, será false
-			 */
-			hayComida = (comidaComprada >= kilosComidosTotal) ? true : false;
-			// Si el número de animales es 0, mostraremos el siguiente mensaje
-			if (numAnimales == 0)
-				System.out.println("No hay animales en la granja que consuman la comida comprada");
-			// En caso contrario...
-			else {
-				// ... y, si el valor de hayComida es true, mostraremos el siguiente mensaje
-				if (hayComida)
-					System.out.println("Hoy disponemos de suficiente alimento para cada animal");
-				else { // ... y, si el valor de hayComida es false...
-						// ... y el valor de comidaComprada es 0, mostraremos el siguiente mensaje
-					if (comidaComprada == 0)
-						System.out.println("Hoy los animales no tienen nada que comer");
-					else { // ... y el valor de comidaComprada no es 0
-							// Establecemos la relación entre las variables
-						kilosComidosPorAnimal = comidaComprada / numAnimales;
-						// Y mostraremos el siguiente resultado
-						System.out.println(
-								"Hoy sería necesario racionarla a " + kilosComidosPorAnimal + " kg por animal");
+				/*
+				 * Si el número se sale del margen le indicamos el error y pediremos otro todas
+				 * las veces necesarias hasta que introduzca un número correcto
+				 */
+				while (kilosComidosTotal <= 0) {
+					System.out.println("Dato incorrecto, sólo validos números mayores que 0");
+					System.out.println("Introduzca otro número");
+					kilosComidosTotal = sc.nextDouble();
+				}
+				/*
+				 * Si la cantidad de comida comprada es igual o superior a la consumida por los
+				 * animales, el valor de hayComida es true; por el contrario, será false
+				 */
+				hayComida = (comidaComprada >= kilosComidosTotal) ? true : false;
+				// Si el número de animales es 0, mostraremos el siguiente mensaje
+				if (numAnimales == 0)
+					System.out.println("No hay animales en la granja que consuman la comida comprada");
+				// En caso contrario...
+				else {
+					// ... y, si el valor de hayComida es true, mostraremos el siguiente mensaje
+					if (hayComida)
+						System.out.println("Hoy disponemos de suficiente alimento para cada animal");
+					else { // ... y, si el valor de hayComida es false...
+							// ... y el valor de comidaComprada es 0, mostraremos el siguiente mensaje
+						if (comidaComprada == 0)
+							System.out.println("Hoy los animales no tienen nada que comer");
+						else { // ... y el valor de comidaComprada no es 0
+								// Establecemos la relación entre las variables
+							kilosComidosPorAnimal = comidaComprada / numAnimales;
+							// Y mostraremos el siguiente resultado
+							System.out.println(
+									"Hoy sería necesario racionarla a " + kilosComidosPorAnimal + " kg por animal");
+						}
 					}
 				}
+			} catch (InputMismatchException e) {
+				System.out.println("El dato introducido no es correcto");
+				sc.nextLine();
 			}
-		} catch (InputMismatchException e) {
-			System.out.println("El dato introducido no es correcto");
-		}
+			
+		} while (compradoCorrecto == false);
 		// Cerramos el Scanner
 		sc.close();
 	}
