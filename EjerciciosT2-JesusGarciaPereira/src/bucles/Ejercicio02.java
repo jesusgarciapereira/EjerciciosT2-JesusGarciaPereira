76f43096d2331d51971768a775464405a2455bd2
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-06 20:38:30 +0100

    02 En proceso

---
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio02.java
@@ -0,0 +1,18 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio02 {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Cantidad de números primos entre 1 y n");
+		System.out.println("Introduzca un valor para n");
+		n = sc.nextInt();
+		
+		}
+
+	}
+
+
