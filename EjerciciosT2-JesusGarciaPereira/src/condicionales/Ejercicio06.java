8a04aab7217540ad6491d4ed1d44b997aeee59c5
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-06 16:53:29 +0100

    06 Con try catch

--- EjerciciosT2-JesusGarciaPereira/src/condicionales/Ejercicio06.java
+++ EjerciciosT2-JesusGarciaPereira/src/condicionales/Ejercicio06.java
@@ -1,10 +1,17 @@
 package condicionales;
 
+import java.util.InputMismatchException;
 import java.util.Random;
 import java.util.Scanner;
 
 public class Ejercicio06 {
 	/*
+	 * SALIDA: 77 + 34 = ¿? | ENTRADA: 11.1 | RESULTADO ESPERADO: El dato
+	 * introducido no es válido
+	 * 
+	 * SALIDA: 41 + 27 = ¿? | ENTRADA: sesenta y ocho | RESULTADO ESPERADO: El dato
+	 * introducido no es válido
+	 * 
 	 * SALIDA: 58 + 28 = ¿? | ENTRADA: 86 | RESULTADO ESPERADO: ¡Correcto! 58 + 28 =
 	 * 86
 	 * 
@@ -16,6 +23,11 @@ public class Ejercicio06 {
 		int aleatorio1, aleatorio2;
 		// Declaramos la variable de la suma que le pediremos al usuario
 		int suma;
+		/*
+		 * Declaramos una variable boolean para determinar si un dato introducido es
+		 * valido
+		 */
+		boolean valido = false;
 		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
 		// Presentamos el programa
@@ -33,13 +45,40 @@ public class Ejercicio06 {
 		aleatorio2 = r.nextInt(1, 100);
 		// Mostramos las dos variables
 		System.out.println(aleatorio1 + " + " + aleatorio2 + " = ¿?");
-		System.out.println("Introduzca la solución");
-		suma = sc.nextInt();
-		if (suma == aleatorio1 + aleatorio2)
-			System.out.print("¡Correcto! ");
-		else
-			System.out.print("Lo siento, la solución es ");
-		System.out.println(aleatorio1 + " + " + aleatorio2 + " = " + (aleatorio1 + aleatorio2));
+		do {
+			try {
+				// Le pedimos al usuario la solución
+				System.out.println("Introduzca la solución");
+				// Leemos el número
+				suma = sc.nextInt();
+				/*
+				 * Si he introducido un dato invalido se ejecutarán directamente las
+				 * instrucciones del catch y el valor de valido seguirá siendo false. Por el
+				 * contrario se asignará como true y se ejecutarán las instrucciones siguientes
+				 */
+				valido = true;
+				// Si es correcto...
+				if (suma == aleatorio1 + aleatorio2)
+					// ... mostrará este mensaje
+					System.out.print("¡Correcto! ");
+				// En caso contrario...
+				else
+					// ... mostrará este otro mensaje
+					System.out.print("Lo siento, la solución es ");
+				// En cualquier caso, siempre mostrará la solución real a continuación
+				System.out.println(aleatorio1 + " + " + aleatorio2 + " = " + (aleatorio1 + aleatorio2));
+				// Si he introducido un dato incorrecto...
+			} catch (InputMismatchException e) {
+				// ... se mostrará el siguiente resultado
+				System.out.println("El dato introducido no es correcto");
+				// Para evitar un bucle infinito
+				sc.nextLine();
+			}
+		} while (valido == false); /*
+									 * Se ejecutarán de nuevo las instrucciones del do, hasta que se introduzca un
+									 * dato valido
+									 */
+		// Cerramos el Scanner
 		sc.close();
 	}
 }
