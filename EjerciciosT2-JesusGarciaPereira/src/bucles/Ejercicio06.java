afb97cb48a757a1c48382a3161b19f4b838ac245
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-07 14:43:18 +0100

    06 En proceso

---
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio06.java
@@ -0,0 +1,17 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio06 {
+
+	public static void main(String[] args) {
+		int n;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Números desde 1 hasta n n-veces");
+		System.out.println("Introduzca un valor para n");
+		n = sc.nextInt();
+		
+
+	}
+
+}
