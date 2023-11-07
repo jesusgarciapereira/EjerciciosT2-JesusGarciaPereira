58c08f15d2b37ae4b5e8a01688bf41fadfb05826
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-07 13:06:10 +0100

    02 Formula perfeccionada

--- EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio02.java
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio02.java
@@ -24,7 +24,7 @@ public class Ejercicio02 {
 				}
 				for (int i = 2; i <= n; i++) {
 					esPrimo = true;
-					for (int j = 2; j < i; j++) {
+					for (int j = 2; j <= Math.sqrt(i); j++) {
 						if (i % j == 0) {
 							esPrimo = false;
 							break;
