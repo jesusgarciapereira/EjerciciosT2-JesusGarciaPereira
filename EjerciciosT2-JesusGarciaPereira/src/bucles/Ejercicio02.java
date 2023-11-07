2bdb4506bb2c6d7a9c80db4ce61dec79fbaf6f0a
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-07 11:55:32 +0100

    02 Hecho sin comentar

--- EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio02.java
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio02.java
@@ -1,29 +1,44 @@
 package bucles;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio02 {
 
 	public static void main(String[] args) {
 		int n;
-		int contadorPrimos=0;
+		int contadorPrimos = 0;
 		boolean esPrimo;
+		boolean correcto = false;
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Cantidad de números primos entre 1 y n");
-		System.out.println("Introduzca un valor para n");
-		n = sc.nextInt();
-		for (int i = 2; i <= n; i++) {
-			esPrimo = true;
-			for (int j = 2; j < i; j++) {
-				if (i % j ==0)
-					esPrimo=false;
+		do {
+			try {
+				System.out.println("Introduzca un valor para n");
+				n = sc.nextInt();
+				correcto = true;
+				while (n < 1) {
+					System.out.println("Número incorrecto, sólo válidos a partir del 1");
+					System.out.println("Introduzca otro número");
+					n = sc.nextInt();
+				}
+				for (int i = 2; i <= n; i++) {
+					esPrimo = true;
+					for (int j = 2; j < i; j++) {
+						if (i % j == 0) {
+							esPrimo = false;
+							break;
+						}
+					}
+					if (esPrimo)
+						contadorPrimos++;
+				}
+				System.out.println("Entre el 1 y el " + n + " hay " + contadorPrimos + " número(s) primo(s)");
+			} catch (InputMismatchException e) {
+				System.out.println("El dato introducido no es correcto");
+				sc.nextLine();
 			}
-			if (esPrimo)
-				contadorPrimos++;
-			
-		}
-		System.out.println(contadorPrimos);
-		}
+		} while (correcto == false);
+		sc.close();
 	}
-
-
+}
