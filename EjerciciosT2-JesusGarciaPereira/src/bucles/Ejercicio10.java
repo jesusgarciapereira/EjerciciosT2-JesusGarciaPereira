dff70269ef9363e8541148ff3b5ab8500541bd0d
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-08 18:39:14 +0100

    10 Hecho sin comentar

---
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio10.java
@@ -0,0 +1,30 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio10 {
+
+	public static void main(String[] args) {
+		int num;
+		int numInverso = 0;
+		int resto;
+		int numAuxiliar;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Capicúa");
+		System.out.println("Introduzca un número mayor que 0");
+		num = sc.nextInt();
+		numAuxiliar = num;
+		while (numAuxiliar != 0) {
+			resto = numAuxiliar % 10;
+			numInverso = numInverso * 10 + resto;
+			numAuxiliar = numAuxiliar / 10;
+
+		}
+		if (num == numInverso)
+			System.out.println("Es capicúa");
+		else
+			System.out.println("No es capicúa");
+		sc.close();
+	}
+
+}
