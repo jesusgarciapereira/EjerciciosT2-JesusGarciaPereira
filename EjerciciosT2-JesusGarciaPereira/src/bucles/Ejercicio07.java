defe40730ad2389e83cecc0f0c1cbaab76fe12e1
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-08 09:07:21 +0100

    Hecho sin comentar

--- EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio07.java
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio07.java
@@ -2,29 +2,29 @@ package bucles;
 
 import java.util.Scanner;
 
-public class Ejercicio06 {
+public class Ejercicio07 {
 
 	public static void main(String[] args) {
 		int n;
 		Scanner sc = new Scanner(System.in);
-		System.out.println("Números desde 1 hasta n n-veces");
+		System.out.println("Pirámide de números de n filas");
 		System.out.println("Introduzca un valor para n");
 		n = sc.nextInt();
-		while (n < 0 || n > 20) {
-			System.out.println("Número incorrecto, sólo validos desde el 0 hasta el 20");
+		while (n < 0) {
+			System.out.println("Número incorrecto, sólo válidos a partir del 0");
 			System.out.println("Introduzca otro número");
 			n = sc.nextInt();
 		}
 		for (int i = 1; i <= n; i++) {
 			for (int j = 1; j <= i; j++) {
-				System.out.print(i);
+				System.out.print(j);
+			}
+			for (int j = i - 1; j >= 1; j--) {
+				System.out.print(j);
 			}
 			System.out.println();
 		}
-		// Si el valor de n es 0...
-		if (n == 0)
-			// .. mostraremos el siguiente mensaje
-			System.out.println("Con n asignado a " + n + " no hay nada que mostrar");
 		sc.close();
 	}
+
 }
