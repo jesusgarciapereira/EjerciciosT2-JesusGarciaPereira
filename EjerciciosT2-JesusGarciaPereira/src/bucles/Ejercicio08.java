b75cd54ae6dbe095bcfda30303c51871522f61d7
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-08 17:15:18 +0100

    08 Terminado sin comentarios

--- EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio08.java
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio08.java
@@ -12,20 +12,25 @@ public class Ejercicio08 {
 		System.out.println("Lectura mayores");
 		System.out.print("Dime un número inicial: ");
 		numInicial = sc.nextInt();
+		contIntroducidos++;
 		do {
 			System.out.print("Dime un número: ");
 			otroNum = sc.nextInt();
-			contIntroducidos++;
-			if (otroNum <= numInicial) {
-				contFallos++;
-				if (otroNum < numInicial)
-					System.out.println("Fallo, es menor");
-				else
-					System.out.println("Fallo, es igual");
+			if (otroNum != 0) {
+				contIntroducidos++;
+				if (otroNum <= numInicial) {
+					contFallos++;
+					if (otroNum < numInicial)
+						System.out.println("Fallo, es menor");
+					else
+						System.out.println("Fallo, es igual");
+				}
 			}
 		} while (otroNum != 0);
+
 		System.out.println("Total números introducidos: " + contIntroducidos);
 		System.out.println("Números fallados: " + contFallos);
+		sc.close();
 	}
 
 }
