7d03087f6385266354e92c743b300cf5607b45d3
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-08 10:19:22 +0100

    08 Terminado sin comentar

---
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio08.java
@@ -0,0 +1,31 @@
+package bucles;
+
+import java.util.Scanner;
+
+public class Ejercicio08 {
+
+	public static void main(String[] args) {
+		int numInicial, otroNum;
+		int contIntroducidos = 0;
+		int contFallos = 0;
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Lectura mayores");
+		System.out.print("Dime un número inicial: ");
+		numInicial = sc.nextInt();
+		do {
+			System.out.print("Dime un número: ");
+			otroNum = sc.nextInt();
+			contIntroducidos++;
+			if (otroNum <= numInicial) {
+				contFallos++;
+				if (otroNum < numInicial)
+					System.out.println("Fallo, es menor");
+				else
+					System.out.println("Fallo, es igual");
+			}
+		} while (otroNum != 0);
+		System.out.println("Total números introducidos: " + contIntroducidos);
+		System.out.println("Números fallados: " + contFallos);
+	}
+
+}
