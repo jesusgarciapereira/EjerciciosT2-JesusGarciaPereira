73e643f8727c67486aa591a8c02f95258c00302e
Jesús <Jess@LAPTOP-8MILMTUM>
2023-11-10 00:31:22 +0100

    DEFINITIVO

--- EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio09.java
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio09.java
@@ -1,24 +1,67 @@
 package bucles;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio09 {
 
+	/*
+	 * ENTRADA: 4.7 | RESULTADO ESPERADO: El dato introducido no es del tipo
+	 * correcto
+	 * 
+	 * ENTRADA: dígitos | RESULTADO ESPERADO: El dato introducido no es del tipo
+	 * correcto
+	 */
+	// ENTRADA: 0 | RESULTADO ESPERADO: Introduzca un número mayor que 0
+	// ENTRADA: 5 | RESULTADO ESPERADO: El número 5 tiene 1 cifras
+	// ENTRADA: 223 | RESULTADO ESPERADO: El número 223 tiene 3 cifras
+	// ENTRADA: 3456543 | RESULTADO ESPERADO: El número 3456543 tiene 7 cifras
+
 	public static void main(String[] args) {
-		int num;
-		int contCifras =0;
+
+		// Declaramos la variables
+		int num = 0;
+		int numAuxiliar;
+		int contCifras = 0;
+
+		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
+
+		// Presentamos el programa
 		System.out.println("Número de dígitos");
-		System.out.println("Introduzca un número mayor que 0");
-		num = sc.nextInt();
-		while(num>0) {
-			num = num /10;
+		System.out.println("-----------------");
+
+		do {
+			try {
+				// Le pedimos al usuario un número y lo leemos
+				System.out.println("Introduzca un número mayor que 0");
+				num = sc.nextInt();
+
+				/*
+				 * Si el usuario ingresa un dato del tipo incorrecto, mostramos un mensaje de
+				 * error y limpiamos el Scanner
+				 */
+			} catch (InputMismatchException e) {
+				System.out.println("El dato introducido no es del tipo correcto");
+				sc.nextLine();
+			}
+
+			// Mientras el número no esté dentro del rango, repetiremos las instrucciones
+		} while (num <= 0);
+
+		// Hacemos una copia del número original para no modificarlo
+		numAuxiliar = num;
+
+		// Bucle while para contar las cifras del número
+		while (numAuxiliar > 0) {
+			numAuxiliar /= 10;
 			contCifras++;
 		}
-			System.out.println(contCifras);
-			sc.close();
-		}
-	
+
+		// Mostramos el resultado
+		System.out.println("El número " + num + " tiene " + contCifras + " cifras");
+
+		// Cerramos el Scanner
+		sc.close();
+	}
 }
-
-
--- EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio10.java
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio10.java
@@ -1,30 +1,84 @@
 package bucles;
 
+import java.util.InputMismatchException;
 import java.util.Scanner;
 
 public class Ejercicio10 {
 
-	public static void main(String[] args) {
-		int num;
-		int numInverso = 0;
-		int resto;
-		int numAuxiliar;
-		Scanner sc = new Scanner(System.in);
-		System.out.println("Capicúa");
-		System.out.println("Introduzca un número mayor que 0");
-		num = sc.nextInt();
-		numAuxiliar = num;
-		while (numAuxiliar > 0) {
-			resto = numAuxiliar % 10;
-			numInverso = numInverso * 10 + resto;
-			numAuxiliar = numAuxiliar / 10;
+	/*
+	 * ENTRADA: 78.3 | RESULTADO ESPERADO: El dato introducido no es del tipo
+	 * correcto
+	 * 
+	 * ENTRADA: capicúa | RESULTADO ESPERADO: El dato introducido no es del tipo
+	 * correcto
+	 *
+	 * ENTRADA: -1 | RESULTADO ESPERADO: Introduzca un número (a partir de 0)
+	 * 
+	 * ENTRADA: 7 | RESULTADO ESPERADO: El inverso de 7 es 7. Por tanto, es capicúa
+	 * 
+	 * ENTRADA: 13 | RESULTADO ESPERADO: El inverso de 13 es 31. Por tanto, no es
+	 * capicúa 
+	 * 
+	 * ENTRADA: 77 | RESULTADO ESPERADO: El inverso de 77 es 77. Por tanto,
+	 * es capicúa 
+	 * 
+	 * ENTRADA: 456 | RESULTADO ESPERADO: El inverso de 456 es 654. Por
+	 * tanto, no es capicúa 
+	 * 
+	 * ENTRADA: 6776 | RESULTADO ESPERADO: El inverso de 6776
+	 * es 6776. Por tanto, es capicúa
+	 * 
+	 */
 
+	public static void main(String[] args) {
+
+		// Declaramos la variables
+		int num = -1;
+		int numInverso = 0;
+		int unidad;
+		int numAuxiliar;
+
+		// Activamos el Scanner
+		Scanner sc = new Scanner(System.in);
+
+		// Presentamos el programa
+		System.out.println("Capicúa");
+		System.out.println("-------");
+
+		do {
+			try {
+
+				// Le pedimos al usuario un número y lo leemos
+				System.out.println("Introduzca un número (a partir de 0)");
+				num = sc.nextInt();
+
+				/*
+				 * Si el usuario ingresa un dato del tipo incorrecto, mostramos un mensaje de
+				 * error y limpiamos el Scanner
+				 */
+			} catch (InputMismatchException e) {
+				System.out.println("El dato introducido no es del tipo correcto");
+				sc.nextLine();
+			}
+
+			// Mientras el número no esté dentro del rango, repetiremos las instrucciones
+		} while (num < 0);
+
+		// Hacemos una copia del número original para no modificarlo
+		numAuxiliar = num;
+
+		// Bucle while para invertir el número
+		while (numAuxiliar > 0) {
+			unidad = numAuxiliar % 10;
+			numInverso = numInverso * 10 + unidad;
+			numAuxiliar = numAuxiliar / 10;
 		}
-		if (num == numInverso)
-			System.out.println("Es capicúa");
-		else
-			System.out.println("No es capicúa");
+
+		// Mostramos el resultado
+		System.out.print("El inverso de " + num + " es " + numInverso + ". ");
+		System.out.println("Por tanto, " + (num == numInverso ? "es capicúa" : "no es capicúa"));
+
+		// Cerramos el Scanner
 		sc.close();
 	}
-
 }
