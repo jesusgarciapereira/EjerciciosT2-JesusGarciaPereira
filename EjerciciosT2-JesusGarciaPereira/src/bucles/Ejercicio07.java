0bfd50b6ad1ec18b288f9063f156c7110ec8b408
jesusgarciapereira <jesus.garciap@iesnervion.es>
2023-11-10 13:19:22 +0100

    DEFINITIVO

--- EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio07.java
+++ EjerciciosT2-JesusGarciaPereira/src/bucles/Ejercicio07.java
@@ -15,39 +15,31 @@ public class Ejercicio07 {
 	// ENTRADA: -1 | RESULTADO ESPERADO: Introduzca un valor para n (mayor que 0)
 	// ENTRADA: 0 | RESULTADO ESPERADO: Con n asignado a 0 no hay nada que mostrar
 	/*
-	 * ENTRADA: 4 | RESULTADO ESPERADO: 1 
-	 *                                  121 
-	 *                                  12321 
-	 *                                  1234321
-	 *                                  
-	 * ENTRADA: 6 | RESULTADO ESPERADO: 1 
-	 *                                  121 
-	 *                                  12321 
-	 *                                  1234321
-	 *                                  123454321
-	 *                                  12345654321
+	 * ENTRADA: 4 | RESULTADO ESPERADO: 1 121 12321 1234321
+	 * 
+	 * ENTRADA: 6 | RESULTADO ESPERADO: 1 121 12321 1234321 123454321 12345654321
 	 */
 	// ENTRADA: 21 | RESULTADO ESPERADO: Introduzca un valor para n (entre 0 y 20)
 
 	public static void main(String[] args) {
-		
+
 		// Declaramos la variable
 		int n = -1;
-		
+
 		// Activamos el Scanner
 		Scanner sc = new Scanner(System.in);
-		
+
 		// Presentamos el programa
 		System.out.println("Pirámide de números de n filas");
 		System.out.println("------------------------------");
-		
+
 		do {
 			try {
-				
+
 				// Le pedimos al usuario un valor para n y lo leemos
 				System.out.println("Introduzca un valor para n (mayor que 0)");
 				n = sc.nextInt();
-				
+
 				/*
 				 * Si el usuario ingresa un dato del tipo incorrecto, mostramos un mensaje de
 				 * error y limpiamos el Scanner
@@ -56,7 +48,7 @@ public class Ejercicio07 {
 				System.out.println("El dato introducido no es del tipo correcto");
 				sc.nextLine();
 			}
-			
+
 			// Mientras el número no esté dentro del rango, repetiremos las instrucciones
 		} while (n < 0);
 
@@ -67,12 +59,17 @@ public class Ejercicio07 {
 
 			// Bucle for para las instrucciones de cada fila
 			for (int i = 1; i <= n; i++) {
-				
+
+				// (Opcional) Bucle for para mostrar los espacios que preceden al primer número
+				for (int j = 1; j <= n - i; j++) {
+					System.out.print(" ");
+				}
+
 				// Bucle for para mostrar números ascendentes
 				for (int j = 1; j <= i; j++) {
 					System.out.print(j);
 				}
-				
+
 				// Bucle for para mostrar números descendentes
 				for (int j = i - 1; j >= 1; j--) {
 					System.out.print(j);
@@ -80,7 +77,7 @@ public class Ejercicio07 {
 				System.out.println();
 			}
 		}
-		
+
 		// Cerramos el Scanner
 		sc.close();
 	}
